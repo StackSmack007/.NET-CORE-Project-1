defa19ef3a21b92091863a4e37ea09614c1a1f96
Nikolay <n_hristov007@abv.bg>
2019-10-18 16:00:50 +0300

    add

--- Junjuria/Junjuria/Junjuria.Services/Services/Contracts/IOrderService.cs
+++ Junjuria/Junjuria/Junjuria.Services/Services/Contracts/IOrderService.cs
@@ -10,13 +10,14 @@
     {
         void AddProductToBasket(ICollection<PurchaseItemDto> basket, int productId, uint ammount);
         void SubtractProductFromBasket(List<PurchaseItemDto> basket, int productId, uint ammount);
+        bool TryCreateOrder(List<PurchaseItemDto> basket,string userId);
+      
         ICollection<ProductWarranty> GetMyWarranties(string userId);
        
         ICollection<OrderOutMinifiedDto> GetMyOrders(string userId);
         ICollection<PurchaseItemDetailedDto> GetDetailedPurchaseInfo(ICollection<PurchaseItemDto> purchases);
         void ModifyCountOfProductInBasket(List<PurchaseItemDto> basket, int productId, uint newAmmount);
         IQueryable<OrderForManaging> GetAllForManaging();
-        bool TryCreateOrder(List<PurchaseItemDto> basket,string userId);
         Task<OrderDetailsOutDto> GetOrderDetailsAsync(string id);
         Task SetStatus(string orderId, Status status);
     }
