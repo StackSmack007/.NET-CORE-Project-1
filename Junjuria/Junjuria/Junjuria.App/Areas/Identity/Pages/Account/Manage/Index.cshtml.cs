cf7f9fd299324dcfdb7e2a6615d96cc83a595b69
Nikolay <n_hristov007@abv.bg>
2019-10-20 22:15:05 +0300

    Introduced and modified the Profile Manage page. No emali confirmations are implemented

--- Junjuria/Junjuria/Junjuria.App/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ Junjuria/Junjuria/Junjuria.App/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
 using System.ComponentModel.DataAnnotations;
-using System.Text.Encodings.Web;
+//using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 
 namespace Junjuria.App.Areas.Identity.Pages.Account.Manage
@@ -28,7 +28,7 @@ namespace Junjuria.App.Areas.Identity.Pages.Account.Manage
 
         public string Username { get; set; }
 
-        public bool IsEmailConfirmed { get; set; }
+        //public bool IsEmailConfirmed { get; set; }
 
         [TempData]
         public string StatusMessage { get; set; }
@@ -45,11 +45,24 @@ namespace Junjuria.App.Areas.Identity.Pages.Account.Manage
             [Phone]
             [Display(Name = "Phone number")]
             public string PhoneNumber { get; set; }
+
+            [Required, MaxLength(256)]
+            public string Address { get; set; }
+
+            [Required, MaxLength(32)]
+            public string Town { get; set; }
+          
+            [MaxLength(64)]
+            public string FirstName { get; set; }
+
+            [MaxLength(64)]
+            public string LastName { get; set; }
+
         }
 
         public async Task<IActionResult> OnGetAsync()
         {
-            var user = await _userManager.GetUserAsync(User);
+            AppUser user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
@@ -64,20 +77,19 @@ namespace Junjuria.App.Areas.Identity.Pages.Account.Manage
             Input = new InputModel
             {
                 Email = email,
-                PhoneNumber = phoneNumber
+                PhoneNumber = phoneNumber,
+                Address = user.Address,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Town = user.Town,
             };
-
-            IsEmailConfirmed = await _userManager.IsEmailConfirmedAsync(user);
-
+            ///  IsEmailConfirmed = await _userManager.IsEmailConfirmedAsync(user);
             return Page();
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
+            if (!ModelState.IsValid) return Page();
 
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
@@ -107,40 +119,48 @@ namespace Junjuria.App.Areas.Identity.Pages.Account.Manage
                 }
             }
 
+            if (user.Address != Input.Address || user.Town != Input.Town || user.FirstName != Input.FirstName || user.LastName != user.LastName)
+            {
+                user.Address = Input.Address;
+                user.Town = Input.Town;
+                user.FirstName = Input.FirstName;
+                user.LastName = Input.LastName;
+                await _userManager.UpdateAsync(user);
+            }
             await _signInManager.RefreshSignInAsync(user);
             StatusMessage = "Your profile has been updated";
             return RedirectToPage();
         }
 
-        public async Task<IActionResult> OnPostSendVerificationEmailAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
+        //public async Task<IActionResult> OnPostSendVerificationEmailAsync()
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return Page();
+        //    }
 
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
+        //    var user = await _userManager.GetUserAsync(User);
+        //    if (user == null)
+        //    {
+        //        return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+        //    }
 
 
-            var userId = await _userManager.GetUserIdAsync(user);
-            var email = await _userManager.GetEmailAsync(user);
-            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-            var callbackUrl = Url.Page(
-                "/Account/ConfirmEmail",
-                pageHandler: null,
-                values: new { userId = userId, code = code },
-                protocol: Request.Scheme);
-            await _emailSender.SendAsync(
-                email,
-                "Confirm your email",
-                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+        //    var userId = await _userManager.GetUserIdAsync(user);
+        //    var email = await _userManager.GetEmailAsync(user);
+        //    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+        //    var callbackUrl = Url.Page(
+        //        "/Account/ConfirmEmail",
+        //        pageHandler: null,
+        //        values: new { userId = userId, code = code },
+        //        protocol: Request.Scheme);
+        //    await _emailSender.SendAsync(
+        //        email,
+        //        "Confirm your email",
+        //        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-            StatusMessage = "Verification email sent. Please check your email.";
-            return RedirectToPage();
-        }
+        //    StatusMessage = "Verification email sent. Please check your email.";
+        //    return RedirectToPage();
+        //}
     }
 }
